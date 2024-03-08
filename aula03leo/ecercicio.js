e2829fb9e93fe3ce882cd158b3b19d530860d539
dudap1nheiro <162368715+dudap1nheiro@users.noreply.github.com>
2024-03-08 17:10:43 -0300

    Add files via upload

---
+++ aula03leo/ecercicio.js
@@ -0,0 +1,27 @@
+const nome1 = 'eduarda'
+const sobrenome = 'pinheiro'
+const minhaIdade = 17
+const estudante = true
+
+console.log('meu nome e', nome1, sobrenome, 'tenho', minhaIdade, 'anos e', estudante)
+
+const serie = 'friends'
+const temporadas = 7
+
+//este comando imprime o tipo do valor
+//pode ser string, number, boolean
+console.log(typeof serie, typeof temporadas)
+
+console,log("minha serie favorita e", serie, typeof serie, "ela tem", temporadas, typeof temporadas)
+
+const idadeNumero = 17
+const idadeTexto = idadeNumero.toString()
+
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
+
+const idadeTexto2 = 13
+const idadeNumero2 = Number (idadeTexto2)
+console.log(typeof idadeTexto2)
+console.log(typeof idadeTexto2)
+
