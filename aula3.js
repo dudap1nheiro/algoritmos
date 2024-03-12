8e7ff0108961c60d70de5a27db8a67a55fc8cd67
dudap1nheiro <162368715+dudap1nheiro@users.noreply.github.com>
2024-03-12 17:20:09 -0300

    Add files via upload

---
+++ aula3.js
@@ -0,0 +1,17 @@
+//Este comando imprime uma mensagem
+console.log("bom dia")
+
+
+const nome = 'eduarda'
+let idade = 17
+const altura = 1.55
+const temperatura = 27
+let souUmBooLean = true
+SouUmBoolean = false
+
+
+console.log("meu nome e", nome,"e tenho ", idade, "anos. ")
+idade = 18
+
+'
+
