fc2d63a35a0be4c606c603812af92a076624d1cc
dudap1nheiro <162368715+dudap1nheiro@users.noreply.github.com>
2024-03-12 16:29:40 -0300

    Add files via upload

---
+++ ex4.js
@@ -0,0 +1,11 @@
+let a = 10
+let b = 25
+let c = null 
+
+c = a // o c tem o valor de a 10
+a = b // a vai pegar o valor do b 25
+b = c // o b passa a ter o valor do c 10
+
+console.log('o novo valor de a e', a)
+console.log('o novo valor de b e', b)
+
